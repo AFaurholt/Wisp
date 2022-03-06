9174d37166617776d7b8298e105d8946ece80ade
Asbjørn Faurholt <a_faurholt@outlook.com>
2022-03-06 14:59:29 +0100

    movement and controls

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -1,31 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerBehavior : MonoBehaviour
 {
-    //cam
-    [SerializeField] private Camera playerCam;
-    [SerializeField] private Vector3 playerCamOffset;
-    //light color
-    [SerializeField] private Light playerLight;
-    [SerializeField] private Color colorSafe;
-    [SerializeField] private Color colorDanger;
-    [SerializeField] private float currentSafetyRange = 0f;
+  //cam
+  [SerializeField] private Camera _playerCam;
+  [SerializeField] private Vector3 _playerCamOffset;
+  //light color
+  [SerializeField] private Light _playerLight;
+  [SerializeField] private Color _colorSafe;
+  [SerializeField] private Color _colorDanger;
+  [SerializeField] private float _currentSafetyRange = 0f;
+  //movement
+  [SerializeField] private CharacterController _playerCc;
+  [SerializeField] private float _terminalVelocity = 20f;
+  [SerializeField] private float _maxSpeed = 10f;
+  [SerializeField] private float _baseSpeed = 1f;
+  [SerializeField] private float _decelSpeed = 0.5f;
+  [SerializeField] private float _dashSpeed = 50f;
+  [SerializeField] private float _dashLengthS = 100f;
+  [SerializeField] private float _currentDashLengthS = 0f;
+  [SerializeField] private Vector2 _moveDirection = Vector2.zero;
+  [SerializeField] private Vector2 _moveDirectionLast = Vector2.zero;
+  [SerializeField] private Vector2 _moveVelocity = Vector2.zero;
 
-    void Start()
+  void Start()
+  {
+    //set offset relative to player
+    _playerCam.transform.position = transform.position + _playerCamOffset;
+  }
+
+  void Update()
+  {
+    _playerLight.color = Color.Lerp(_colorSafe, _colorDanger, _currentSafetyRange);
+  }
+
+  void FixedUpdate()
+  {
+    Vector2 move = Vector2.Scale(_moveDirection, new Vector2(_baseSpeed, _baseSpeed));
+    _moveVelocity += move;
+    _moveVelocity = Vector2.ClampMagnitude(_moveVelocity, _maxSpeed);
+
+    if(Mathf.Epsilon > _moveDirection.sqrMagnitude)
     {
-        //set offset relative to player
-        playerCam.transform.position = transform.position + playerCamOffset;
+      _moveVelocity = Vector2.MoveTowards(_moveVelocity, Vector2.zero, _decelSpeed);
     }
 
-    void Update()
-    {
-        playerLight.color = Color.Lerp(colorSafe, colorDanger, currentSafetyRange);
-    }
+    _playerCc.Move(_moveVelocity * Time.deltaTime);
+    transform.position = new Vector3(transform.position.x, transform.position.y, 0);
 
-    void FixedUpdate()
-    {
-        
-    }
+
+    //update delta move
+    _moveDirectionLast = _moveDirection;
+  }
+
+  public void OnMove(InputAction.CallbackContext cbc)
+  {
+    _moveDirection = cbc.ReadValue<Vector2>();
+  }
+
+  private float NormalizeF(float val, float min, float max)
+  {
+    return (val - min) / (max - min);
+  }
 }
